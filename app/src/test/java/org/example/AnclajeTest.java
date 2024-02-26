f2ce60ccc2cf8f2fd66e5a3c55cf0f109c850167
joel <joelfigueiridomolares@gmail.com>
2024-02-26 11:04:51 +0100

    creamos la clase anclaje

--- app/src/test/java/org/example/AnclajeTest.java
+++ app/src/test/java/org/example/AnclajeTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import org.example.Bicicleta;
-
 public class AnclajeTest {
 
     @Test
