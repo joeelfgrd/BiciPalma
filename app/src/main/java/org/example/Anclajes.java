25bea507c7e19e52cbecc4c4344efacbafeb0435
joel <joelfigueiridomolares@gmail.com>
2024-02-27 14:12:05 +0100

    Cambios en  algoritmo de anclajes

--- app/src/main/java/org/example/Anclajes.java
+++ app/src/main/java/org/example/Anclajes.java
@@ -1,25 +1,46 @@
 package org.example;
 
 public class Anclajes {
-    private int numAnclajes;
+    
+    
+    private final Anclaje[] anclajes;
+    
+        Anclajes(int numAnclajes) {
+            this.anclajes = new Anclaje[numAnclajes];
+            crearAnclajes();
+        }
 
-    public Anclajes(int numAnclajes) {
-        this.numAnclajes = numAnclajes;
-    }
-    public String NumAnclajesAString() {
-        return String.valueOf(this.numAnclajes);
-    }
+        private void crearAnclajes() {
+            for (int i = 0; i < anclajes.length; i++) {
+                this.anclajes[i] = new Anclaje();
+            }
+        }
+        Anclaje[] anclajes() {
+            return this.anclajes;
+        }
 
-    @Override
-    public String toString() {
-        return "Numero de anclajes: " + this.numAnclajes;
-    }
-    public void ocuparAnclaje() {
+        
+        String NumAnclajesAString() {
+            return String.valueOf(this.numAnclajes);
+        }
+
+        @Override
+        public String toString() {
+            return "Numero de anclajes: " + this.numAnclajes;
+        }
+        
+        int numAnclajes() {
+            return this.anclajes.length;
+        }
+
+
+        void ocuparAnclaje() {
         this.numAnclajes--;
-    }
-    public Boolean isAnclajeOcupado() {
+        }
+        boolean isAnclajeOcupado() {
         return this.numAnclajes > 0;
-    }
+        }
+        public
 }
 
 
--- app/src/test/java/org/example/AnclajesTest.java
+++ app/src/test/java/org/example/AnclajesTest.java
@@ -2,19 +2,23 @@ package org.example;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class AnclajesTest {
+
 
     @Test
     public void toStringTest() {
         Anclajes anclajes = new Anclajes(6);
         assertEquals("Numero de anclajes: 6", anclajes.toString());
     } 
-    
+
     @Test
-    public void ocuparAnclajeTest() {
+    public void isAnclajeOcupadoTest() {
         Anclajes anclajes = new Anclajes(6);
+        assertFalse(anclajes.isAnclajeOcupado());
         anclajes.ocuparAnclaje();
-        assertEquals("Numero de anclajes: 5", anclajes.toString());
+        assertTrue(anclajes.isAnclajeOcupado());
     }
 }
