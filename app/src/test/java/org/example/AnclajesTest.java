0b1475afdd8f570f046cfa00d476b2863f14c318
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-02-29 19:21:15 +0100

    Anclajes test corregido

--- app/src/test/java/org/example/AnclajesTest.java
+++ app/src/test/java/org/example/AnclajesTest.java
@@ -1,7 +1,6 @@
 package org.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
